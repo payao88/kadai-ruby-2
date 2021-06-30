0bd7e4c7d3dca590fac6a330c0cc757de54ed6c1
paya <r0772624@yahoo.co.jp>
2021-06-30 14:15:12 +0000

    kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,20 +1,22 @@
-def fizzbuzz
-  num_max = 100
-  num = 1
-  while num <= num_max do
-    
+def fizzbuzz(num)
     if (num % 3 == 0) && (num % 5 == 0)
-    puts 'FizzBuzz'
+    'FizzBuzz'
     elsif num % 3 == 0
-    puts 'Fizz'
+    'Fizz'
     elsif num % 5 == 0
-    puts 'Buzz'
+    'Buzz'
     else
-    puts num.to_s
+    num.to_s
     end
-    
-    num+= 1
-  end  
-end  
+end 
 
-puts fizzbuzz()
+
+#puts fizzbuzz(1)
+#puts fizzbuzz(3)
+#puts fizzbuzz(5)
+#puts fizzbuzz(15)
+
+num_max = 100
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end  
